4306657ccceea5b34b292ead425d0b367e15e140
sachind3 <desai.sachin45@gmail.com>
2022-06-03 18:26:55 +0530

    wip

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
-console.log("test");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/goals", require("./routes/goalRoutes"));
+
+app.listen(port, () => {
+  console.log(`Server started in port ${port}`);
+});
